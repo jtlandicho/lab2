3d3a5d2d51ae5c24d85e31bacceb584ed02f5c4b
John Jorel Landicho <jtlandicho@apc.edu.ph>
2024-01-13 23:08:16 +0800

    JavaScript Lesson 5 - Operators, Assignment, Arithmetic & Math

--- week5/script.js
+++ week5/script.js
@@ -4,3 +4,8 @@ document.getElementById("dateandtime").innerHTML = d;
 
 let itexperiencetext = "I also play variety of games, whether online or offline."
 document.getElementById("ITExperience").innerHTML = itexperiencetext
+
+let x = 10;
+let y = 9;
+let age = x + y;
+document.getElementById("age").innerHTML = age;
