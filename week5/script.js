7100b70f00732879ea9262e438d891b960b64e67
John Jorel Landicho <jtlandicho@apc.edu.ph>
2024-01-13 23:44:13 +0800

    JavaScript Lesson 9 - Events, Precedence & Conditionals

--- week5/script.js
+++ week5/script.js
@@ -9,3 +9,15 @@ let x = 10;
 let y = 9;
 let age = x + y;
 document.getElementById("age").innerHTML = age;
+
+var clickbutton = document.getElementById("button");
+var hiddenContent = document.getElementById("content");
+
+    // Add a click event listener to the button
+clickbutton.addEventListener("click", function() {
+	if (hiddenContent.style.display === "none"){
+		hiddenContent.style.display = "block"
+	} else {
+		hiddenContent.style.display = "none"
+	}
+});
