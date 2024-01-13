dc47ba884b9d8ede2002015a900d7f931690867f
John Jorel Landicho <jtlandicho@apc.edu.ph>
2024-01-13 23:00:06 +0800

    JavaScript Lesson 6 - Strings

--- week5/script.js
+++ week5/script.js
@@ -1,3 +1,6 @@
 //My first comment in JavaScript
 const d = new Date("2023-01-13");
 document.getElementById("dateandtime").innerHTML = d;
+
+let itexperiencetext = "I also play variety of games, whether online or offline."
+document.getElementById("ITExperience").innerHTML = itexperiencetext
