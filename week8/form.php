82cc1f549bd9dc197fb73c624b82f5d892de8711
jtlandicho <152581325+jtlandicho@users.noreply.github.com>
2024-02-08 22:04:43 +0800

    Update form.php

--- week8/form.php
+++ week8/form.php
@@ -126,7 +126,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO myguests (name, email, website, comment, gender)
+$sql = "INSERT INTO jtlandicho_myguests (name, email, website, comment, gender)
 VALUES ('$name', '$email', '$website', '$comment', '$gender')";
 
 if ($conn->query($sql) === TRUE) {
