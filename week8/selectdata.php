5222bac5bed14b763c4341b68a2a1df6341816db
jtlandicho <152581325+jtlandicho@users.noreply.github.com>
2024-02-08 22:57:24 +0800

    Update selectdata.php

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -1,8 +1,15 @@
 <?php
+// For Xampp Localhost
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "myDB";
+
+// For socitcloud
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "myDB";
+$username = "webprogmi221";
+$password = "g_6bCitLu.ljMK*m";
+$dbname = "webprogmi221";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
